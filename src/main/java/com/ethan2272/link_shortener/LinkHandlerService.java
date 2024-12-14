0551fd55f81feeeff1abb50d3f1792d5e1a579e1
Ethan <ethanl22702@gmail.com>
2024-12-14 16:52:05 -0500

    added link randomization and redirection

--- src/main/java/com/ethan2272/link_shortener/LinkHandlerService.java
+++ src/main/java/com/ethan2272/link_shortener/LinkHandlerService.java
@@ -5,13 +5,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class LinkHandlerService {
     private LinkMappingRepository LinkRepo;
+    private final int KEY_LENGTH = 5;
+    private final String characterSet = "abcdefghijklmnopqrstuvwxyz" +
+                                        "ABCDEFGHIJKLMNOPQRESTUVWXYZ";
 
     public LinkHandlerService(LinkMappingRepository LinkRepo) {
         this.LinkRepo = LinkRepo;
     }
 
-    public LinkMapping shortenAndSaveLink(String originalLink) {
-        LinkMapping linkMapping = new LinkMapping("shortlink", originalLink);
+    public LinkMapping saveLink(String originalLink) {
+        String shortKey;
+        do {
+            shortKey = generateShortKey();
+        } while (LinkRepo.existsById(shortKey));
+
+        LinkMapping linkMapping = new LinkMapping(shortKey, originalLink);
         LinkRepo.save(linkMapping);
         return linkMapping;
     }
@@ -19,5 +27,16 @@ public class LinkHandlerService {
     public String getOriginalLink(String shortenedLink) {
         LinkMapping linkMapping = LinkRepo.getReferenceById(shortenedLink);
         return linkMapping.getOriginalLink();
+    }
+
+    private String generateShortKey() {
+        StringBuilder sb = new StringBuilder();
+        int characterSetSize = characterSet.length();
+        for(int i = 0; i < KEY_LENGTH; i++) {
+            int characterIndex = (int) (Math.random() * characterSetSize);
+            sb.append(characterSet.charAt(characterIndex));
+        }
+
+        return sb.toString();
     }
 }
--- src/main/java/com/ethan2272/link_shortener/LinkShortenerController.java
+++ src/main/java/com/ethan2272/link_shortener/LinkShortenerController.java
@@ -1,10 +1,12 @@
 package com.ethan2272.link_shortener;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LinkShortenerController {
@@ -17,12 +19,13 @@ public class LinkShortenerController {
 
     @PostMapping("/shorten")
     public LinkMapping shortenLink(@RequestParam(name="link")String link) {
-        LinkMapping linkMapping = LHS.shortenAndSaveLink(link);
+        LinkMapping linkMapping = LHS.saveLink(link);
         return linkMapping;
     }
 
-    @GetMapping("/{shortenedLink}")
-    public String redirectShortenedLink(@PathVariable String shortenedLink) {
-        return LHS.getOriginalLink(shortenedLink);
+    @GetMapping("/{shortenedLink:^[^\\.]+$}")
+    public ModelAndView redirectShortenedLink(@PathVariable String shortenedLink, ModelMap model) {
+        String redirectLink = LHS.getOriginalLink(shortenedLink);
+        return new ModelAndView("redirect:" + redirectLink, model);
     }
 }
